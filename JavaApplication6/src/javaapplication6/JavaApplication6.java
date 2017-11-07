47a08655e3e2031a67d41a49622703d157076fef
Baciu <baciu@wp.pl>
2017-11-07 15:32:09 +0100

    nowa klasa

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package javaapplication6;
 
 import java.io.BufferedReader;
@@ -8,21 +12,17 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
+/**
+ *
+ * @author student
+ */
 public class JavaApplication6 {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Podaj nazwe pliku");
-        String fileName = "";
-        Scanner sc = new Scanner(System.in);
-        fileName = sc.nextLine();
-        FileReader fr = null;
-        try {
-            fr = new FileReader(fileName);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(JavaApplication6.class.getName()).log(Level.SEVERE, null, ex);
-        }
-	BufferedReader br = new BufferedReader(fr);
+        
     }
     
 }
