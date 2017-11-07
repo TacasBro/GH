50203139f01f87324a636ac71b45195e8350e32c
Kusy <kusykryst@gmail.com>
2017-11-07 15:25:42 +0100

    no comment

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication6;
 
 import java.io.BufferedReader;
@@ -12,15 +8,9 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author student
- */
+
 public class JavaApplication6 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Podaj nazwe pliku");
         String fileName = "";
